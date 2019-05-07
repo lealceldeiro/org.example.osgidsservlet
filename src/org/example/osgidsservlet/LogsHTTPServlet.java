27f65420ad2a25fb7b06660eae6d78e4e4ab7cd9
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2019-05-07 18:21:07 +0300

    Initial commit

---
+++ src/org/example/osgidsservlet/LogsHTTPServlet.java
@@ -0,0 +1,28 @@
+package org.example.osgidsservlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = Servlet.class, property = { "alias:String=/gamelogs" }, immediate = true)
+public class LogsHTTPServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2302372200114479484L;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			resp.setContentType("text/plain");
+			resp.getWriter().println("It works!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		resp.setStatus(200);
+	}
+}
